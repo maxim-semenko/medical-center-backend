c943e5b5673e15f5f9fe37a9c1984abd95079b20
unknown <soler.03@mail.ru>
2022-02-27 18:15:04 +0300

    111

--- src/controller/disease.controller.ts
+++ src/controller/disease.controller.ts
@@ -1,36 +1,16 @@
-import {Body, Controller, Delete, Get, HttpCode, Param, Post, Put} from '@nestjs/common';
-import {UserService} from '../service/user.service';
-import {UserEntity} from "../entity/user.entity";
+import {Controller,  Get} from '@nestjs/common';
+import {DiseaseService} from '../service/disease.service';
+import {DiseaseEntity} from "../entity/disease.entity";
 
-@Controller('api/v1/users')
-export class UserController {
-    constructor(private readonly userService: UserService) {
+@Controller('api/v1/diseases')
+export class DiseaseController {
+    constructor(private readonly diseaseService: DiseaseService) {
     }
 
     @Get()
-    findAll(): Promise<UserEntity[]> {
-        return this.userService.findAll();
+    findAll(): Promise<DiseaseEntity[]> {
+        return this.diseaseService.findAll();
     }
-
-    @Get('/:id')
-    findById(@Param("id") id: number): Promise<UserEntity> {
-        return this.userService.findById(id);
-    }
-
-    @Post('')
-    @HttpCode(201)
-    create(@Body() userEntity: UserEntity): Promise<UserEntity> {
-        return this.userService.create(userEntity);
-    }
-
-    @Put('/:id')
-    update(@Param("id") id: number, @Body() userEntity: UserEntity): Promise<UserEntity> {
-        return this.userService.update(id, userEntity);
-    }
-
-    @Delete('/:id')
-    delete(@Param("id") id: number): void {
-        this.userService.deleteById(id);
-    }
-
 }
+
+
