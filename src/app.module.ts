7829cc1b339a5b1c264e083fb05550bf248546f1
pro100-Max <maks.semenko@gmail.com>
2022-03-22 21:10:47 +0300

    MEDICAL_CENTRE-0020_UPDATE fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,13 +7,12 @@ import {AppointmentModule} from "./module/appointment.module";
 import {DiseaseModule} from "./module/disease.module";
 import {EmployeeModule} from "./module/employee.module";
 import {MedicalCardModule} from "./module/medicalCard.module";
-import {RoleModule} from "./module/role.module";
 import {UserAccessModule} from "./module/userAccess.module";
 import {VaccineModule} from "./module/vaccine.module";
 
 @Module({
     imports: [TypeOrmModule.forRoot(), AppointmentModule, DiseaseModule, EmployeeModule, MedicalCardModule,
-        RoleModule, UserModule, UserAccessModule, VaccineModule],
+        UserModule, UserAccessModule, VaccineModule],
     controllers: [AppController],
     providers: [AppService],
 })
