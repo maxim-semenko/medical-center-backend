8d6e448b464a2e61a16b45a760281be965926b32
Valery_Harahliad <Valery_Harahliad@epam.com>
2022-03-26 11:50:13 +0300

    MedicalCentre-0014 start work with authentication

--- src/module/user.module.ts
+++ src/module/user.module.ts
@@ -3,11 +3,13 @@ import {TypeOrmModule} from "@nestjs/typeorm";
 import {UserEntity} from "../entity/user.entity";
 import {UserController} from "../controller/user.controller";
 import {UserService} from "../service/user.service";
+import {UserAccessService} from "../service/userAccess.service";
+import {UserAccessEntity} from "../entity/userAccess.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([UserEntity])],
+    imports: [TypeOrmModule.forFeature([UserEntity, UserAccessEntity])],
     controllers: [UserController],
-    providers: [UserService],
+    providers: [UserService, UserAccessService],
 })
 export class UserModule {
 }
